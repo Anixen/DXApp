12cac407a581cf78cda61430bd34e08f7b25bbb2
Anixen <anixen-dev@laposte.net>
2020-05-21 22:28:36 +0200

    refactor : test Timer in main()

--- ModelViewer/src/main.cpp
+++ ModelViewer/src/main.cpp
@@ -7,9 +7,26 @@
 #include <windows.h>
 #include <iostream>
 
+#include "Timer.h"
 #include "LoggerConsole.h"
 
 //int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+int main()
+{
+	LoggerConsole logger(true, SeverityDebug);
+
+	Timer timer;
+	timer.init();
+
+	Sleep(20);
+
+	// We should have a little bit (< 1 ms) more than the sleep duration
+	float elaspedTime = timer.getElapsedTime();
+	LogMessage(SeverityDebug, "elapsedTime : " + std::to_string(elaspedTime))
+}
+
+
+/*
 int main()
 {
 	//std::cout << "Hello world !" << std::endl;
@@ -25,3 +42,11 @@ int main()
 
 	return 0;
 }
+//*/
+
+/*
+int main()
+{
+	std::cout << "Hello world !" << std::endl;
+}
+//*/
